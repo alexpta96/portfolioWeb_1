07717813d1a9338803ab194a933371f941f66968
Alex Phan <alexpta96@gmail.com>
2020-10-23 08:38:04 +0700

    Complete project

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,2 +1,1 @@
 Tutorial link: https://www.youtube.com/watch?v=ZFQkb26UD1Y&t=10468s
-time : 03:05:00
--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 const header = document.querySelector('#header');
 const hamburger = header.querySelector('.hamburger');
+const links = header.querySelectorAll('.header .navbar .nav-list ul li a');
 
 hamburger.addEventListener('click', () => {
     header.classList.toggle('active');
+});
+document.addEventListener('scroll', () => {
+    let y_position = window.scrollY;
+    if(y_position > 250) {
+        header.style.backgroundColor = '#29232c';
+    } else {
+        header.style.backgroundColor = '';
+    }
+});
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        header.classList.remove('active');
+    });
 });
